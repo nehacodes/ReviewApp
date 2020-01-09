c3b5d7d723d19c91f62a1af8c4f232aec5dea593
Neha Sharma <nehashrm315@gmail.com>
2020-01-09 12:19:11 +0530

    Update Program.cs

--- aspnet-core-dotnet-core/Program.cs
+++ aspnet-core-dotnet-core/Program.cs
@@ -18,7 +18,7 @@ namespace aspnet_core_dotnet_core
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
                 .Build();
-
+//comment
             host.Run();
         }
     }
